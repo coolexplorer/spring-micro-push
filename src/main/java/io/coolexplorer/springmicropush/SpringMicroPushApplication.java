3ea5607e90d3b31a0045b742d794161671941191
seunkim <seunkim@ea.com>
2022-02-27 19:27:28 -0800

    Initial Project

---
+++ src/main/java/io/coolexplorer/springmicropush/SpringMicroPushApplication.java
@@ -0,0 +1,13 @@
+package io.coolexplorer.springmicropush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/coolexplorer/springmicropush/SpringMicroPushApplicationTests.java
@@ -0,0 +1,13 @@
+package io.coolexplorer.springmicropush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
